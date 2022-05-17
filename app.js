70ea1d8e3a7e690fc743a7bbd125c7f525c20515
jomit1612 <jomitthomas1612@gmail.com>
2022-05-17 15:12:07 +0100

    tested endpoint

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const { getTopics } = require("./controller/topics.controller");
+const { getArticleById } = require("./controller/articles.controller");
 
 const app = express();
 
 app.get("/api/topics", getTopics);
+app.get("/api/articles/:article_id", getArticleById);
+
 app.use("/*", (req, res, next) => {
-  res.status(404).send({ msg: "Not found" });
+  res.status(404).send({ msg: "not found" });
+});
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "bad request" });
+  } else {
+    next(err);
+  }
+});
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(404).send({ msg: "not found" });
+  } else {
+    next(err);
+  }
 });
 
+app.use((err, req, res, next) => {
+  res.status(500).send({ msg: "server error" });
+});
 module.exports = app;
---
+++ controller/articles.controller.js
@@ -0,0 +1,11 @@
+const { fetchArticleById } = require("../models/articles.models.js");
+
+exports.getArticleById = (req, res, next) => {
+  fetchArticleById(req.params.article_id)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
